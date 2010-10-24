57479272b6dfc228b21f8481d14dae57a9f506e8
Matt Light <matt.light@lightdatasys.com>
2010-10-24 05:32:37 +0000

    Added Driver, TvStation, Track, and Season NASCAR read accessor classes.
    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/public_html/svn/lidsys/framework/trunk@12 41f6023a-4bf8-4d25-a79e-dc8bb3566cb0

---
+++ tests/Lidsys/Nascar/Accessor/Reader/SeasonTest.php
@@ -0,0 +1,89 @@
+<?php
+
+
+
+class Lidsys_Nascar_Accessor_Reader_SeasonTest
+extends Lidsys_Test_TestCase
+{
+	protected $_group;
+	protected $_mapper;
+	protected $_cache;
+	protected $_stream;
+	
+	protected $_reader;
+	
+	
+	
+	public function setUp()
+	{
+		$conn		= $this->getDbConnection('testing');
+		$table_set	= new Mephex_Db_TableSet('nascar');
+		
+		$this->loadXmlDataSetIntoDb($conn, 'Lidsys_Nascar', 'stream');
+		
+		$this->_group	= new Lidsys_Nascar_Accessor_Group($conn, $table_set);
+		$this->_mapper	= new Lidsys_Nascar_Mapper_Season($this->_group);
+		$this->_cache	= new Lidsys_Nascar_Cache_Season();
+		$this->_stream	= new Lidsys_Nascar_Stream_Reader_Season($conn, $table_set);
+		
+		$this->_reader		= new Lidsys_Nascar_Accessor_Reader_Season(
+			$this->_group,
+			$this->_mapper,
+			$this->_cache,
+			$this->_stream
+		);
+	}
+	
+	
+	
+	public function testReaderAccessorCanBeInitialized()
+	{
+	}
+	
+
+	
+	public function testSeasonCanBeReadById()
+	{
+		$entity	= $this->_reader->read(new Mephex_Model_Criteria_Array(array('id' => 3)));
+		
+		$this->assertEquals('3',					$entity->getId());
+		$this->assertEquals('1',					$entity->getSeries()->getId());
+		$this->assertEquals('2009',					$entity->getYear());
+		$this->assertEquals('2009 Cup',				$entity->getTitle());
+		$this->assertEquals('5',					$entity->getMaxPickCount());
+		$this->assertEquals('26',					$entity->getChaseRaceNumber());
+		$this->assertEquals('12',					$entity->getChaseDriverCount());
+	}
+	
+
+	
+	public function testSeasonCanBeReadByYearAndSeriesKeyName()
+	{
+		$entity	= $this->_reader->read(new Mephex_Model_Criteria_Array(array('year' => '2010', 'series' => 'truck')));
+		
+		$this->assertEquals('8',					$entity->getId());
+		$this->assertEquals('3',					$entity->getSeries()->getId());
+		$this->assertEquals('2010',					$entity->getYear());
+		$this->assertEquals('2010 Truck',			$entity->getTitle());
+		$this->assertEquals('3',					$entity->getMaxPickCount());
+		$this->assertEquals('0',					$entity->getChaseRaceNumber());
+		$this->assertEquals('0',					$entity->getChaseDriverCount());
+	}
+	
+
+	
+	public function testSeasonCanBeReadByYearAndSeries()
+	{
+		$series	= new Lidsys_Nascar_Entity_Series();
+		$series->setKeyName('national');
+		$entity	= $this->_reader->read(new Mephex_Model_Criteria_Array(array('year' => '2010', 'series' => $series)));
+		
+		$this->assertEquals('7',					$entity->getId());
+		$this->assertEquals('2',					$entity->getSeries()->getId());
+		$this->assertEquals('2010',					$entity->getYear());
+		$this->assertEquals('2010 Nationwide',		$entity->getTitle());
+		$this->assertEquals('4',					$entity->getMaxPickCount());
+		$this->assertEquals('0',					$entity->getChaseRaceNumber());
+		$this->assertEquals('0',					$entity->getChaseDriverCount());
+	}
+}
