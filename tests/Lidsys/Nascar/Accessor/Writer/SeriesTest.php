6958581540c297e8b58a0438e2d64fa87d80fa92
Matt Light <matt.light@lightdatasys.com>
2011-07-03 01:35:25 +0000

    Created writer accessor for series.
    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/public_html/svn/lidsys/framework/trunk@19 41f6023a-4bf8-4d25-a79e-dc8bb3566cb0

---
+++ tests/Lidsys/Nascar/Accessor/Writer/SeriesTest.php
@@ -0,0 +1,92 @@
+<?php
+
+
+
+class Lidsys_Nascar_Accessor_Writer_SeriesTest
+extends Lidsys_Test_TestCase
+{
+	protected $_conn;
+	protected $_table_set;
+	
+	protected $_group;
+	protected $_mapper;
+	protected $_cache;
+	protected $_istream;
+	protected $_ostream;
+	
+	protected $_reader;
+	protected $_writer;
+	
+	
+	
+	public function setUp()
+	{
+		$this->_conn		= $this->getDbConnection('testing');
+		$this->_table_set	= new Mephex_Db_TableSet('nascar');
+		
+		$this->loadXmlDataSetIntoDb($this->_conn, 'Lidsys_Nascar', 'stream');
+		
+		$this->_group	= new Lidsys_Nascar_Accessor_Group($this->_conn, $this->_table_set);
+		$this->_mapper	= new Lidsys_Nascar_Mapper_Series($this->_group);
+		$this->_cache	= new Lidsys_Nascar_Cache_Series();
+		
+		$this->_istream	= new Lidsys_Nascar_Stream_Reader_Series($this->_conn, $this->_table_set);
+		$this->_ostream	= new Lidsys_Nascar_Stream_Writer_Series($this->_conn, $this->_table_set);
+		
+		$this->_reader		= new Lidsys_Nascar_Accessor_Reader_Series(
+			$this->_group,
+			$this->_mapper,
+			$this->_cache,
+			$this->_istream
+		);
+		$this->_writer	= new Lidsys_Nascar_Accessor_Writer_Series(
+			$this->_group,
+			$this->_mapper,
+			$this->_cache,
+			$this->_ostream
+		);		
+	}
+	
+	
+	
+	public function testAccessorCanBeInitialized()
+	{
+	}
+	
+	
+	
+	public function testEntityCanBeUpdated()
+	{
+		$series	= $this->_reader->read(new Mephex_Model_Criteria_Array(array('id' => 1)));
+		
+		$this->assertEquals('Sprint Cup', $series->getTitle());
+		
+		$series->setTitle('Nextel Cup');
+		$this->_writer->write($series);
+		
+		$iter	= $this->_istream->read(new Mephex_Model_Criteria_Array(array('id' => 1)));
+		$iter->rewind();
+		$rawSeries	= $iter->current();
+		
+		$this->assertEquals('Nextel Cup', $rawSeries['name']);
+	}
+	
+	
+	
+	public function testEntityCanBeCreated()
+	{
+		$series	= new Lidsys_Nascar_Entity_Series();
+		$series->setKeyname('lower');
+		$series->setTitle('Lower Division Series');
+		$series->setShortTitle('lower');
+		$this->_writer->write($series);
+		
+		$this->assertNotNull($series->getId());
+		
+		$iter	= $this->_istream->read(new Mephex_Model_Criteria_Array(array('id' => $series->getId())));
+		$iter->rewind();
+		$rawSeries	= $iter->current();
+		
+		$this->assertEquals('Lower Division Series', $rawSeries['name']);
+	}
+}
